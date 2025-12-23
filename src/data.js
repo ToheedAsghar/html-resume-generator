21a46912b8436e8198c7ea9fd85ad0467f38547b
Toheed Asghar <toheedasghar1@gmail.com>
2025-12-24 00:04:56 +0500

    initial commit

---
+++ src/data.js
@@ -0,0 +1,82 @@
+module.exports = {
+    professionalSummary: {
+        summary: "Computer Science Graduate and software Engineer seeking to grow in Full Stack and Enterprise Application Development"
+    },
+    personalInfo: {
+        name: "Toheed Asghar",
+        email: "toheedasghar@hotmail.com",
+        github: "github.com/toheedasghar",
+        linkedin: "linkedin.com/in/toheed-asghar"
+    },
+    education: {
+        university: "Punjab University College of Information Technlogy (PUCIT)",
+        degree: "BSCS",
+        date: "2021 - 2025",
+        cgpa: 3.5
+    },
+    professionalExperience: [
+        {
+            company: "Techlogix (Pvt.) Ltd.",
+            role: "Software Engineer",
+            date: "Oct 2025 - Present",
+            description: [
+                "Currently in training and contributing to ALM, and PFT project",
+                "Working with basic ETL Tasks and gaining familarity with dataflows, data warehousing, and financial risk modules."
+            ]
+        },
+        {
+            company: "Revolving Games",
+            role: "Full Stack Developerc",
+            date: "July 2025 - Sept 2025",
+            description: [
+                "Built features in production apps using React (frontend), Redux Toolkit (RTK) for state management, and Sequelize as ORM, gained exposure to Express.js on the backend.",
+                "Quickly onboarded to ongoing projects and contributed user-facing improvements.",
+                "Proposed and implemented an optimization to minimize redundant state reset methods, which improved state consistency and reduced UI glitches."
+            ]
+        },
+        {
+            company: "Netsol Technologies Ltd.",
+            role: ".NET Developer Intern",
+            date: "Aug 2024 - Sept 2024",
+            description: [
+                "Created and tested RESTful Web APIs in ASP.NET; implemented small backend modules and queries.",
+                "Learned the basics of Angular and built simple UI components and integrations."
+            ]
+        }
+    ],
+    projects: [
+        {
+            name: "Virtual Fashion Fit",
+            link: "https://github.com/ToheedAsghar/Virtual-Fashion-Fit",
+            description: [
+                "Developed a virtual try-on application leveraging computer vision and machine learning to simulate realistic garment fitting, applying structured reasoning for human-computer interaction.",
+                "Worked with vision models (VITON) for garment detection and overlay, demonstrating applied AI problem-solving in a real-world scenario"
+            ]
+        },
+        {
+            name: "Gadget Gourmet E-commerce Platform",
+            link: "https://github.com/ToheedAsghar/Gadget-Gourmet-DotNET",
+            description: [
+                "Built a full-stack e-commerce platform for modern kitchen gadgets, focusing on a clean architecture approach for maintainable code. ",
+                "Integrated secure checkout, inventory management, and role-based authentication using ASP.NET Core and Blazor to deliver a functional product.",
+                "Ensured a responsive, user-friendly UI with Bootstrap and seamless data management using Entity Framework Core."
+            ]
+        },
+        {
+            name: "Smart Meters in London",
+            link: "https://github.com/ToheedAsghar/Smart-Meters-London-Analytics",
+            description: [
+                "Analyzed energy consumption patterns from a large-scale London smart meter dataset, preprocessing over 5 million records using Python, Pandas, and NumPy for modeling readiness.",
+                "Performed feature engineering on time-series and weather data to enhance predictive accuracy of energy usage trends."
+            ]
+        }
+    ],
+    skills: {
+        programmingLanguages: "C, C++, C#, Python, Javascript",
+        frontend: "React, Redux Toolkit (RTK), Tailwind CSS, SCSS, HTML/CSS, Bootstrap",
+        backend: "Express.js , ASP.NET Core, Entity Framework",
+        databases: "SQL Server, Oracle SQL, Sequelize",
+        tools: "Git, Github, Docker, Visual Studio, Vscode, Pycharm",
+        additional: "Angular (Basic Exposure"
+    }
+}
